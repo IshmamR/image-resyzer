e4acee55a58b59bb73e60a1e813c11ad587cd36d
IshmamR <ishmam785@gmail.com>
2021-04-23 20:38:55 +0600

    looks fine to me

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = express();
 const multer = require('multer');
 const imageSize = require('image-size');
 const sharp = require('sharp');
+const zip = require('express-zip');
 
 // node js internal modules
 const fs = require('fs');
@@ -14,21 +15,27 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use(express.static('user'));
+// app.use(express.static('user'));
 
 
 app.get('/', (req, res) => {
 	res.status(200);
 	fs.createReadStream(__dirname+'/index.html').pipe(res);
+});
+// redirect to home
+app.get('/processImage', (req, res) => {
+	res.redirect('/');
 })
 
 
 // make directory for uploaded images if doesn't exist already
 var dir = "user";
-var subDir = "user/uploads";
+var subDir = "user/uploads"; // uploaded images
+var resDir = "user/processed"; // processed images
 if(!fs.existsSync(dir)) {
 	fs.mkdirSync(dir);
 	fs.mkdirSync(subDir);
+	fs.mkdirSync(resDir);
 }
 
 // Multer
@@ -53,7 +60,6 @@ var imageFilter = function (req, file, cb) {
 	}
 };
 var upload = multer({ storage: storage, fileFilter: imageFilter });
-// var upload = multer({ dest: 'useruploads/' });
 //console.log(upload.array('file'));
 
 
@@ -65,43 +71,80 @@ app.post('/processImage', upload.array('file'), (req, res) => {
 	width = req.body.width;
 	height = req.body.height;
 	// console.log(JSON.stringify(req.body));
+	
 	if ( isNaN(width) || isNaN(height) || width.trim() === '' || height.trim() === '' ) {
-		for(let i = 0; i < req.files.length; i++) {
-			dimensions = imageSize(req.files[i].path);
-			console.log(req.files[i].path);
-			width = parseInt(dimensions.width);
-			height = parseInt(dimensions.height);
-
-			processImage(Number(width), Number(height), req, res, req.files[i]);
-		}
+		processImage(req, res, req.files);
 	} else {
-		for (let i = 0; i < req.files.length; i++) {
-			processImage(Number(width), Number(height), req, res, req.files[i]);
-		}
+		processImage(req, res, req.files);
 	}
 	
 })
 
-
+var arrayForZip = [];
 // Using Sharp to resize and convert image
-function processImage(width, height, req, res, imageFile) {
-	outputFilePath = Date.now() + "output." + format;
-	// console.log(width)
-	if (imageFile) {
-		sharp(imageFile.path)
-		.resize(width, height)
-		.toFile(outputFilePath, (err, info) => {
-			if (err) throw err;
-			res.download(outputFilePath, (err) => {
+function processImage(req, res, imageFile) {
+	// console.log('imageFile: '+imageFile)
+	
+	for(let i = 0; i < imageFile.length; i++) {
+		
+		dimensions = imageSize(imageFile[i].path);
+		width = parseInt(dimensions.width);
+		height = parseInt(dimensions.height);
+
+		fileName = imageFile[i].originalname.split('.').slice(0, -1).join('.'); // file name without extension
+		fileOutput = fileName + Date.now() + "." + format;
+		outputFilePath = path.join(resDir, fileOutput); // "user/processed/test.png"
+		// console.log(imageFile[i]);
+
+		if (imageFile[i]) {
+			sharp(imageFile[i].path)
+			.resize(width, height)
+			.toFile(outputFilePath, (err, info) => {
 				if (err) throw err;
-				fs.unlinkSync(imageFile.path);
-				fs.unlinkSync(outputFilePath);
+				// console.log(info)
+				fs.unlinkSync(imageFile[i].path); // deletes file from uploads after processing
+				if (i === imageFile.length - 1) {
+					downloadImage(req, res);
+				}
 			});
-		});
+		}
+		
+		zipObject = { path: outputFilePath, name: fileOutput };
+		arrayForZip.push(zipObject);
 	}
+	
 }
 
-
+// download zip or single image
+function downloadImage (req, res) {
+	// console.log("zipArray: "+ arrayForZip.length)
+	var numberOfFiles = arrayForZip.length;
+	
+	// Single image download
+	if (numberOfFiles < 2 && numberOfFiles === 1) { 
+		res.download(arrayForZip[0].path, (err) => {
+			if (err) {
+				// console.log('Path: '+arrayForZip[0].path)
+				throw err;
+			}
+			fs.unlinkSync(arrayForZip[0].path); // delete file after download
+			arrayForZip.length = 0;
+		});
+	}
+	
+	// Multiple images (zip) download
+	if (numberOfFiles >= 2) { 
+		res.zip(arrayForZip, 'resized.zip', (err) => {
+			if (err) throw err;
+			// delete files after download
+			for (let i = 0; i < arrayForZip.length; i++) {
+				fs.unlinkSync(arrayForZip[i].path);
+			}
+			arrayForZip.length = 0;
+		});
+			
+	}
+}
 
 
 const PORT = process.env.PORT || 3000;
